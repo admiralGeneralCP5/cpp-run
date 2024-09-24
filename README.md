f2243933672d283f3fe2d314de40f75933d7d1d6
admiralGeneralCP5 <149021332+admiralGeneralCP5@users.noreply.github.com>
2024-09-24 14:07:13 -0400

    C++ version for mac/linux and windows.

---
+++ README.md
@@ -0,0 +1,15 @@
+# cpp-run
+cpp-run is a short script that automates using the gcc compiler for C++. It is made for working on single file programs, when you need to be able to compile and run a program repeatedly.
+
+This script:
+1. compiles the .cpp file
+2. runs the executable
+3. deletes the executable
+
+## Instructions
+
+1. Compile the script (cpp version) 
+Example: ```g++ cpp-run-mac-linux.cpp -o cpprun```
+2. Add the script to PATH
+3. Run with a C++ file. Example: ```cpprun helloworld.cpp```
+
---
+++ cpp-run-win.cpp
@@ -0,0 +1,52 @@
+/*
+Originally written in python by admiralGeneralCP5
+Converted to C++ by codellama LLM
+Edited by admiralGeneralCP5
+*/
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  // Check if an argument was given
+  if (argc != 2) {
+    std::cout << "A C++ file with a .cpp extension is required." << std::endl;
+    return 1;
+  }
+
+  // Get the filename from the first command-line argument
+  std::string filename = argv[1];
+
+  // Check if the filename ends with ".cpp"
+  if (filename.substr(filename.length() - 4) != ".cpp") {
+    std::cout << "A C++ file with a .cpp extension is required." << std::endl;
+    return 1;
+  }
+
+  // Compile the file using g++
+  std::string cmd = "g++ " + filename + " -o " + filename.substr(0, filename.length() - 4);
+  int status = system(cmd.c_str());
+
+  if (status != 0) {
+    std::cout << "Error compiling file." << std::endl;
+    return 1;
+  }
+
+  // Run the executable file that was created
+  cmd = ".\\" + filename.substr(0, filename.length() - 4);
+  status = system(cmd.c_str());
+
+  if (status != 0) {
+    std::cout << "Error running executable." << std::endl;
+    return 1;
+  }
+  
+  // Delete executable after it runs
+  cmd = "del " + filename.substr(0, filename.length() - 4);
+  status = system(cmd.c_str());
+
+  if (status != 0) {
+    std::cout << "Error deleting executable." << std::endl;
+    return 1;
+  }
+  return 0;
+}
